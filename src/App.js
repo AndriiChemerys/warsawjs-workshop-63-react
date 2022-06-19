d8e4863eb100a49d592eef91455dee174860f656
Andrii <andriichemerys@gmail.com>
2022-06-19 12:25:40 +0000

    Refactoring

--- src/App.js
+++ src/App.js
@@ -7,15 +7,7 @@ function App() {
   const [preview, setPreview] = useState();
   const [text1, setText1] = useState("Text1");
   const [text2, setText2] = useState("Text2");
-  const onTextInputChange = ({ target }) => {
-    setText1(target.value);
-  };
-  const onTextDownInputChange = ({ target }) => {
-    setText2(target.value);
-  };
-  const onFileInputChange = ({ target }) => {
-    setFile(target.files[0]);
-  };
+
   useEffect(() => {
     if (!file) {
       setPreview(undefined);
@@ -33,9 +25,18 @@ function App() {
         {preview && (
           <Capture img={preview} textTop={text1} textBottom={text2} />
         )}
-        <input type={"file"} onChange={onFileInputChange} />
-        <input type={"text"} onChange={onTextInputChange} />
-        <input type={"textBottom"} onChange={onTextDownInputChange} />
+        <input
+          type={"text"}
+          onChange={({ target }) => setText1(target.value)}
+        />
+        <input
+          type={"text"}
+          onChange={({ target }) => setText2(target.value)}
+        />
+        <input
+          type={"file"}
+          onChange={({ target }) => setFile(target.files[0])}
+        />
       </header>
     </div>
   );
